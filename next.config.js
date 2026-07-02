e86c6e6a75c2870e0b12e0486d21c0553e47f41d
Work <work@Kais-MacBook-Pro.local>
2026-07-01 23:12:49 -0700

    add /app

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/app',
+        destination: 'https://pointschest-app.vercel.app/', // TODO: Replace with your actual Expo web app URL
+      },
+      {
+        source: '/app/:path*',
+        destination: 'https://pointschest-app.vercel.app/:path*', // TODO: Replace with your actual Expo web app URL
+      },
+    ]
+  },
+}
 
 module.exports = nextConfig
