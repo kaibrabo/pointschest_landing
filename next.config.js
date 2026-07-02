27031d83f949805ae34dbbcf526c5cbfab7ffc94
Work <work@Kais-MacBook-Pro.local>
2026-07-01 23:31:49 -0700

    update url endpoints for /app

--- next.config.js
+++ next.config.js
@@ -1,11 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirects() {
+  async rewrites() {
     return [
       {
         source: '/app',
-        destination: 'https://pointschest-app.vercel.app/',
-        permanent: false,
+        destination: 'https://pointschest-app.vercel.app/', 
+      },
+      {
+        source: '/app/:path*',
+        destination: 'https://pointschest-app.vercel.app/:path*', 
+      },
+      {
+        source: '/_expo/:path*',
+        destination: 'https://pointschest-app.vercel.app/_expo/:path*', 
+      },
+      {
+        source: '/assets/:path*',
+        destination: 'https://pointschest-app.vercel.app/assets/:path*', 
       },
     ]
   },
