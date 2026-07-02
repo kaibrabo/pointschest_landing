0753501e87440884a52cec6410dfc854da8bef6a
Work <work@Kais-MacBook-Pro.local>
2026-07-01 23:27:25 -0700

    add /app (fix 2)

--- next.config.js
+++ next.config.js
@@ -1,18 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
+  async redirects() {
     return [
       {
         source: '/app',
-        destination: 'https://pointschest-app.vercel.app/', // TODO: Replace with your actual Expo web app URL
-      },
-      {
-        source: '/app/:path*',
-        destination: 'https://pointschest-app.vercel.app/:path*', // TODO: Replace with your actual Expo web app URL
-      },
-      {
-        source: '/_expo/:path*',
-        destination: 'https://pointschest-app.vercel.app/_expo/:path*', 
+        destination: 'https://pointschest-app.vercel.app/',
+        permanent: false,
       },
     ]
   },
